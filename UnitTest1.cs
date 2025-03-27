78b4f6ef09b3e2c817b8d0f45babe94a2ca34ee3
Ahmad Raza <a.raza@plazz.ag>
2025-03-27 17:07:22 +0100

    Initial commit - SpecFlow BDD Playwright setup

---
+++ UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace SauceDemoTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
